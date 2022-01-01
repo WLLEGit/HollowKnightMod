b53d8cb4cc56b94301509a314f5281166de9f695
WLLE <342970955@qq.com>
2022-01-01 19:09:32 +0800

    Init Project

---
+++ HKHeroControl/GrimmCtrl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using TranCore;
+using HutongGames.PlayMaker;
+using HutongGames.PlayMaker.Actions;
+
+namespace HKHeroControl
+{
+    public class GrimmCtrl : MonoBehaviour
+    {
+        public TranAttach TranAttach => gameObject.GetTranAttach();
+        tk2dSpriteAnimation animation = null;
+        Rigidbody2D rig = null;
+        DefaultActions defaultActions = null;
+
+        GameObject todo;
+
+        void OnCollisionEnter2D(Collision2D collision) => OnCollisionStay2D(collision);
+        void OnCollisionStay2D(Collision2D collision)
+        {
+        }
+    }
+}
---
+++ HKHeroControl/HeroController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modding;
+using UnityEngine;
+
+#region Acknowledgement
+/// <summary>
+/// TranCore.dll    作者：空洞实验室    项目地址：https://github.com/HKLab/TranCore   
+/// </summary>
+#endregion
+
+namespace HKHeroControl
+{
+    public class HeroController : Mod
+    {
+        public GameObject FalseKnightGO = null;
+        public GameObject GrimmGO = null;
+        public override List<(string, string)> GetPreloadNames()
+        {
+            var res = new List<(string, string)>();
+            foreach (var val in configs.Values)
+                res.Add((val.HeroScene, val.HeroAssertPath));
+            return res;
+        }
+        public override void Initialize(Dictionary<string, Dictionary<string, UnityEngine.GameObject>> preloadedObjects)
+        {
+            //InitGameObject<FalseKnightCtrl>(in preloadedObjects, "FalseKnight", out FalseKnightGO);
+            InitGameObject<GrimmCtrl>(in preloadedObjects, "Grimm", out GrimmGO);
+        }
+
+        private void InitGameObject<T>(in Dictionary<string, Dictionary<string, GameObject>> preloadedObjects, string name, out GameObject go) where T : Component
+        {
+            var config = configs[name];
+            go = UnityEngine.Object.Instantiate(
+                preloadedObjects[config.HeroScene][config.HeroAssertPath]
+                );
+
+            go.transform.parent = null;
+            UnityEngine.Object.DontDestroyOnLoad(go);
+            go.AddComponent<T>();
+        }
+        private struct ConfigType
+        {
+            public string HeroScene;
+            public string HeroAssertPath;
+            public ConfigType(string scene, string assert)
+            {
+                HeroScene = scene;
+                HeroAssertPath = assert;
+            }
+        }
+        private Dictionary<string, ConfigType> configs = new Dictionary<string, ConfigType>
+        {
+            {"FalseKnight", new ConfigType("", "") },
+            {"Grimm", new ConfigType("", "") }
+        };
+
+    }
+}
