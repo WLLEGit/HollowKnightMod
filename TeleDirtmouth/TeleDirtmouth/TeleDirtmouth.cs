95293a06ca25948bd2b1f7364b2141f3f3c4aecb
WLLE <342970955@qq.com>
2022-01-10 19:29:51 +0800

    添加快捷键传送mod

---
+++ TeleDirtmouth/TeleDirtmouth/TeleDirtmouth.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Modding;
+using UnityEngine;
+using GlobalEnums;
+
+namespace TeleDirtmouth
+{
+    public class TeleDirtmouth : Mod
+    {
+        public override void Initialize()
+        {
+            ModHooks.Instance.HeroUpdateHook += ModHooks_HeroUpdateHook;
+        }
+
+        private void ModHooks_HeroUpdateHook()
+        {
+            if(Input.GetKeyDown(KeyCode.F1))
+            {
+                TeleToDirtmouth();
+            }
+        }
+
+        string name = "Dirtmouth";
+        string areaName = "Cliffs";
+        MapZone mapZone = MapZone.TOWN;
+        string style = "Right";
+        string sceneName = "Town";
+        string respawnMarker = "RestBench";
+        int respawnType = 1;
+        Dictionary<string, double> specificOffset = new Dictionary<string, double>
+        {
+            { "x", 0.0} ,
+            { "y", -0.68},
+            { "z", 0.0 }
+        };
+
+        void TeleToDirtmouth()
+        {
+            PlayerData.instance.respawnScene = sceneName;
+            PlayerData.instance.respawnMarkerName = respawnMarker;
+            PlayerData.instance.respawnType = respawnType;
+            PlayerData.instance.mapZone = mapZone;
+            GameManager.instance.StartCoroutine(Respawn());
+        }
+
+        private static IEnumerator Respawn()
+        {
+            GameManager.instance.SaveGame();
+            GameManager.instance.TimePasses();
+            UIManager.instance.UIClosePauseMenu();
+
+            // Collection of various redundant attempts to fix the infamous soul orb bug
+            HeroController.instance.TakeMPQuick(PlayerData.instance.MPCharge); // actually broadcasts the event
+            HeroController.instance.SetMPCharge(0);
+            PlayerData.instance.MPReserve = 0;
+            HeroController.instance.ClearMP(); // useless
+            PlayMakerFSM.BroadcastEvent("MP DRAIN"); // This is the main fsm path for removing soul from the orb
+            PlayMakerFSM.BroadcastEvent("MP LOSE"); // This is an alternate path (used for bindings and other things) that actually plays an animation?
+            PlayMakerFSM.BroadcastEvent("MP RESERVE DOWN");
+
+            // Set some stuff which would normally be set by LoadSave
+            HeroController.instance.AffectedByGravity(false);
+            HeroController.instance.transitionState = HeroTransitionState.EXITING_SCENE;
+            if (HeroController.instance != null)
+            {
+                if (HeroController.instance.cState.onConveyor || HeroController.instance.cState.onConveyorV || HeroController.instance.cState.inConveyorZone)
+                {
+                    HeroController.instance.GetComponent<ConveyorMovementHero>()?.StopConveyorMove();
+                    HeroController.instance.cState.inConveyorZone = false;
+                    HeroController.instance.cState.onConveyor = false;
+                    HeroController.instance.cState.onConveyorV = false;
+                }
+
+                HeroController.instance.cState.nearBench = false;
+            }
+            GameManager.instance.cameraCtrl.FadeOut(CameraFadeType.LEVEL_TRANSITION);
+
+            yield return new WaitForSecondsRealtime(0.5f);
+
+            // Actually respawn the character
+            GameManager.instance.SetPlayerDataBool(nameof(PlayerData.atBench), false);
+            // Allow the player to have control if they warp to a non-bench while diving or cdashing
+            if (HeroController.instance != null)
+            {
+                HeroController.instance.cState.superDashing = false;
+                HeroController.instance.cState.spellQuake = false;
+            }
+            GameManager.instance.ReadyForRespawn(false);
+
+            yield return new WaitWhile(() => GameManager.instance.IsInSceneTransition);
+
+            // Revert pause menu timescale
+            Time.timeScale = 1f;
+            GameManager.instance.FadeSceneIn();
+
+            // We have to set the game non-paused because TogglePauseMenu sucks and UIClosePauseMenu doesn't do it for us.
+            GameManager.instance.isPaused = false;
+
+            // Restore various things normally handled by exiting the pause menu. None of these are necessary afaik
+            GameCameras.instance.ResumeCameraShake();
+            if (HeroController.instance != null)
+            {
+                HeroController.instance.UnPause();
+            }
+            MenuButtonList.ClearAllLastSelected();
+
+            //This allows the next pause to stop the game correctly
+            TimeController.GenericTimeScale = 1f;
+
+            // Restores audio to normal levels. Unfortunately, some warps pop atm when music changes over
+            GameManager.instance.actorSnapshotUnpaused.TransitionTo(0f);
+            GameManager.instance.ui.AudioGoToGameplay(.2f);
+        }
+    }
+}
